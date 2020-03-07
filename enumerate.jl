9b4e3c65a312fe85284ab6fa4703581799b8a37a
Parker J. Rule <parker.rule@tufts.edu>
2020-03-06 22:05:39 -0500

    Add fully logged version of enumeration

---
+++ enumerate.jl
@@ -0,0 +1,147 @@
+using Distributed
+@everywhere using Combinatorics
+@everywhere using Random
+@everywhere include("./Grid.jl")
+@everywhere using .Grid
+@everywhere struct Extremes
+    min_share::Float64
+    max_share::Float64
+    min_voters::Set{Array{Int}}
+    max_voters::Set{Array{Int}}
+    id::Int
+    hash::UInt
+end
+include("./dedupe.jl")
+
+const batch_size = 10000
+const channel_size = 1 #16384
+const n_voters = 12
+
+@everywhere function eval_voter_dists(jobs, results)
+    while true
+        voter_dists = take!(jobs)
+        println("got block of size ", size(voter_dists, 2))
+        max_share = 0
+        min_share = 1
+        max_voters = Set([])
+        min_voters = Set([])
+        voter_hash = hash(Set(voter_dists[:, i] for i in 1:size(voter_dists, 2)))
+        for i in 1:size(voter_dists, 2)
+            voters = voter_dists[:, i]
+            share = expected_seat_share(voters)
+            if share > max_share
+                max_share = share
+                max_voters = Set([voters])
+            elseif share == max_share
+                push!(max_voters, voters)
+            end
+            if share < min_share
+                min_share = share
+                min_voters = Set([voters])
+            elseif share == min_share
+                push!(min_voters, voters)
+            end
+        end
+        extremes = Extremes(min_share, max_share, min_voters, max_voters, rand(Int), voter_hash)
+        println("putting extremes with min_share: ", min_share, "\tmax_share: ", max_share)
+        put!(results, extremes)
+    end
+end
+
+function add_combos(jobs::RemoteChannel, batch_size::Int, n_voters::Int)
+    batch_count = 0
+    batch_idx = 1
+    batch = zeros(Int, n_voters, batch_size)
+    for c in combinations(1:ensemble.size, n_voters)
+        if !combo_duplicated(c, minimum(c), ensemble.width)
+            batch[:, batch_idx] = c
+            if batch_idx == 10
+                println("tenth combo is ", c)
+                println("first ten:")
+                println(batch[:, 1:10]')
+            end
+            batch_idx += 1
+            if batch_idx > batch_size
+                println("putting batch ", batch_count)
+                put!(jobs, batch)
+                println("put batch ", batch_count)
+                voter_hash = hash(Set(batch[:, i] for i in 1:size(batch, 2)))
+                println("putting vote dists with set hash ", voter_hash)
+                batch_idx = 1
+                batch_count += 1 
+            end
+            if batch_count == 21
+                break
+            end
+        end
+    end
+    # Handle remaining partial batch
+    put!(jobs, batch[:, 1:batch_idx - 1])
+    batch_count += 1
+    batch_count
+end
+
+function main()
+    jobs = RemoteChannel(()->Channel{Array{Int, 2}}(channel_size))
+    results = RemoteChannel(()->Channel{Extremes}(channel_size))
+    # Start workers.
+    for p in workers()
+        remote_do(eval_voter_dists, p, jobs, results)
+    end
+
+    max_share = 0
+    min_share = 1
+    max_voters = Set([])
+    min_voters = Set([])
+    @async add_combos(jobs, batch_size, n_voters)
+    #add_combos(jobs, batch_size, n_voters)
+    for i in 1:20
+        batch_extremes = take!(results)
+        println("[batch ", i, "]")
+        println("\tmin share: ", batch_extremes.min_share)
+        println("\tmax share: ", batch_extremes.max_share)
+        println("\trandom result ID: ", batch_extremes.id)
+        println("\tvoter dist hash:  ", batch_extremes.hash)
+        println("\tmin voter dists:")
+        for v in batch_extremes.min_voters
+            println("\t\t", v)
+        end
+        println("\tmax voter dists:")
+        for v in batch_extremes.max_voters
+            println("\t\t", v)
+        end
+
+        if batch_extremes.min_share < min_share
+            min_share = batch_extremes.min_share
+            min_voters = batch_extremes.min_voters
+        elseif batch_extremes.min_share == min_share
+            min_voters = union(min_voters, batch_extremes.min_voters)
+        end
+        if batch_extremes.max_share > max_share
+            max_share = batch_extremes.max_share
+            max_voters = batch_extremes.max_voters
+        elseif batch_extremes.max_share == max_share
+            max_voters = union(max_voters, batch_extremes.max_voters)
+        end
+    end
+    println("min share: ", min_share)
+    println("max share: ", max_share)
+    println("min voter dists:")
+    for v in min_voters
+        println(v)
+    end
+    println("max voter dists:")
+    for v in max_voters
+        println(v)
+    end
+    println(max_voters)
+
+    # Kill workers.
+    for wid in workers()
+        if wid != 1
+            rmprocs(wid)
+        end
+    end
+end
+
+main()
